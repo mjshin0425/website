c737a1a1424a3fac5537ab11df16be26e18f67a6
mjshin0425 <junis0425@gmail.com>
2024-02-14 13:21:34 +0900

    [UPDATE] 검색기능 생성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   console.log("i run all the time");
   const iRunOnlyOnce = () => {
     console.log("i run only once.");
@@ -10,8 +14,14 @@ function App() {
   useEffect(() => {
     console.log("call the api");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+      console.log("SEARCH FOR", keyword);
+  }, []);
+
   return (
     <div>
+      <input onChange={onChange} type="text" placeholder="Search here.." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
